e1d17979ab889a0ced293af5302f82d49086b7d5
trionfordring <fordring@fordring.icu>
2022-12-06 14:04:06 +0800

    chore(web): 优化发包流程

--- prototyper-web/gulpfile.ts
+++ prototyper-web/gulpfile.ts
@@ -1,11 +1,24 @@
-import { parallel } from 'gulp';
+import { parallel, series } from 'gulp';
 
 import { run } from './build/utils/process';
+import { withTaskName } from './build/utils/withTaskName';
 
 export const core = () => run('pnpm run -F @prototyper/core build');
 
 export const changelog = () => run('changeset add');
 
 export const release = () => run('changeset version');
+
+export const publishCore = withTaskName(
+  'publish:core',
+  series(
+    () => run('pnpm login --registry https://registry.npmjs.org'),
+    () => run('pnpm run build:prod core'),
+    () =>
+      run(
+        'pnpm publish -F core --registry https://registry.npmjs.org --access=public'
+      )
+  )
+);
 
 export default parallel(core);
